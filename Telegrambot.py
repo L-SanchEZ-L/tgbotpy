24d649a2634ba8ceade9e63044c043ee1a2c6ce1
root <root@localhost.localdomain>
2019-10-18 12:45:05 +0300

    first commit

---
+++ Telegrambot.py
@@ -0,0 +1,26 @@
+import pyowm
+import telebot
+ 
+owm = pyowm.OWM('12dae92ed6a64b461ffdedb00c6d4f77', language = 'ua')
+bot = telebot.TeleBot("701199951:AAGm5uAUEDfnXiDtmex0GLizwqcXwDR37s0")
+
+@bot.message_handler(content_types=['text'])
+
+
+def send_echo(message):
+  try:
+      observation = owm.weather_at_place(message.text) 
+      w = observation.get_weather()
+      temp = w.get_temperature('celsius')['temp']
+
+      answer = ' В місці ' + message.text + ' зараз ' + w.get_detailed_status() + '\n'
+      answer += 'Температура в районі ' + str(temp) + 'C°' + '\n\n'
+
+  except pyowm.exceptions.api_response_error.NotFoundError:
+    answer = 'Ви шо зовсім тупі?'
+    
+  finally:  
+       
+   bot.send_message(message.chat.id, answer)
+
+bot.polling( none_stop = True )
