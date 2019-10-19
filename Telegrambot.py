856edd8f5ccd9c58ee53fc9a789d5ed5abaeab92
L-SanchEZ-L <56363506+L-SanchEZ-L@users.noreply.github.com>
2019-10-19 11:58:27 +0300

    Update Telegrambot.py

--- Telegrambot.py
+++ Telegrambot.py
@@ -26,24 +26,4 @@ def send_echo(message):
 
 bot.polling( none_stop = True )
 
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://dashboard.heroku.com/apps/weather-tg-botpy")
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.  
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
-    bot.remove_webhook()
-    bot.polling(none_stop=True)
 
